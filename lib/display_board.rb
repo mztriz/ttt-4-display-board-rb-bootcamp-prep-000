6f63726a11fea545a560b111e7622532373f7a5c
Ava Barron <mztriz@gmail.com>
2018-09-26 16:46:07 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,11 +2,15 @@ default_board = Array.new(9," ")
 
 def display_board(board=default_board)
   vert_row_top = ["  "," | ", " ", " | ", "  "]
+  vert_row_mid = ["  "," | ", " ", " | ", "  "]
+  vert_row_end = ["  "," | ", " ", " | ", "  "]
   hori_row = "-----------"
+  
   puts vert_row_top.join.to_s
   puts hori_row
-  puts vert_row.join.to_s
-  puts board
+  puts vert_row_mid.join.to_s
+  puts hori_row
+  puts vert_row_end.join.to_s
 end
 
 display_board(default_board)
