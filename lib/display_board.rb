4ff4b43ead4086b184c6c7939aa818465171f3f5
Ava Barron <mztriz@gmail.com>
2018-09-26 17:15:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,6 +3,10 @@ default_board = Array.new(9," ")
 def display_board(board=default_board)
   vert_row = ["   |   |   ","   |   |   ","   |   |   "]
   hori_row = "-----------"
+  
+  
+  puts vert_row[0].join.to_s
+  
   =begin
   # top
   if (check_places(board[0..2]))
@@ -21,6 +25,7 @@ def display_board(board=default_board)
     puts vert_row[3].join.to_s
   end
   =end
+  
 end
 
 def check_places(row)
