725df29942839348c1c3fbb6332a796ef72e0290
Ava Barron <mztriz@gmail.com>
2018-09-26 16:44:10 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 default_board = Array.new(9," ")
 
 def display_board(board=default_board)
-  vert_row = [" O ","|", " X ", "|", " "]
+  vert_row = [" "," | ", " ", " | ", " "]
   hori_row = "-----------"
   puts vert_row.join.to_s
   puts hori_row
